2f26012eb00c6925bb3c4905f220d146e4058259
1997sanket <1997sanket@gmail.com>
2020-05-25 22:15:59 +0530

    Adding Day 8 of JavaScript

---
+++ JavaScript own/Day 8 (Class with a closure function for generating ID)/script.js
@@ -0,0 +1,83 @@
+
+//Closure function to automatically generate employee ID.
+
+getId = (function () {
+
+    let ID = 0;
+    return function () {
+
+        return ID = ID + 1;
+
+    }
+
+})();
+
+
+
+class Employee {
+
+
+    constructor(name, salary) {
+        this.name = name;
+        this.salary = salary;
+        this.id = getId();
+    }
+
+
+
+
+    get empID() {
+        return this.id;
+    }
+
+    get empName() {
+        return this.name;
+    }
+
+    set empName(name) {
+        this.name = name;
+    }
+
+    get empSalary() {
+        return this.salary;
+    }
+
+    set empSalary(salary) {
+        this.salary = salary;
+    }
+
+
+    netSalary() {
+        return this.salary - (0.1 * this.salary);
+    }
+}
+
+
+let e1 = new Employee('Sanket', 100000);
+let e2 = new Employee('Aditya', 90000);
+let e3 = new Employee('Dhruv', 80000);
+
+
+console.log(e1.empID, e1.empName, e1.empSalary, e1.netSalary());
+console.log(e2.empID, e2.empName, e2.empSalary, e2.netSalary());
+console.log(e3.empID, e3.empName, e3.empSalary, e3.netSalary());
+
+
+
+
+
+// class Car {
+//     constructor(brand) {
+//         this.carname = brand;
+//     }
+//     get cnam() {
+//         return this.carname;
+//     }
+//     set cnam(x) {
+//         this.carname = x;
+//     }
+// }
+
+// let mycar = new Car("Ford");
+
+// console.log(mycar.carname);
